45591ce097e00881816d6a895db6762d85766ac8
BikoPython <stevennyandoya@gmail.com>
2021-01-30 00:49:47 +0300

    additions

---
+++ Second Edition/ looping-with-v-for/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        characters: ['Mario','Luigi','Yoshi','Bowser'],
+        ninjas: [
+            {name:'Rush',age:5},
+            {name:'Vitz',age:3},
+            {name:'Biko',age:29}
+        ]
+    },
+    methods: {
+        
+    }
+});
---
+++ Second Edition/intro-to- components/app.js
@@ -0,0 +1,23 @@
+Vue.component('greeting', {
+    template: '<p>Hey there, I am a re-usable component</p>'
+})
+
+new Vue({
+    el: '#vue-app-one',
+    data: {
+        
+    },
+    methods: {
+        
+    }
+});
+
+new Vue({
+    el: '#vue-app-two',
+    data: {
+        
+    },
+    methods: {
+        
+    }
+});
