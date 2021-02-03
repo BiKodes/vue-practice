28ded90bcc8619ef075a22618e9190728ff33a11
BikoPython <stevennyandoya@gmail.com>
2021-02-03 19:07:33 +0300

    slots added

---
+++ first-edition/slots/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <form-helper>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>Valuable information aboit the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required>
+        <input type="password" placeholder="password" required>
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+import formHelper from './components/formHelper.vue';
+
+export default {
+ 
+
+ components: {
+   'form-helper': formHelper
+ },
+  data () {
+    return {
+
+
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+
+</style>
