ade15d5f21636c4ffdfec49dbff90ee7917c8364
BikoPython <stevennyandoya@gmail.com>
2021-02-10 14:06:46 +0300

    additions: route-parameters

---
+++ first-edition/route-parameters/src/components/showBlogs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <input type="text" v-model="search" placeholder="search blogs"/>
+    <div v-for="blog in filteredBlogs" class="single-blog">
+       
+       <router-link v-bind:to="'/blog/'+ blog.id"><h2>{{ blog.title | to-uppercase}}</h2></router-link> 
+        <article>{{ blog.body | snippet }}</article>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import searchMixin from '../mixins/searchMixin'
+
+export default {
+
+  data() {
+    return {
+        blogs: [],
+        search:''
+    }
+  },
+  methods: {
+   
+  },
+    created() {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+        this.blogs = data.body.slice(0,10);
+    });  
+    },
+
+    computed: {
+     
+    },
+    mixins: [searchMixin]
+}
+</script>
+
+<style>
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
